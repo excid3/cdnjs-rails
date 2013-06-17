53cf07cf0be3d8d5a2c5cec651b717609a28f2db
Chris Hough <chris@noconformity.com>
2013-06-16 22:19:16 -0700

    demo app setup, time to work readme

--- lib/cdnjs-rails/view_helpers.rb
+++ lib/cdnjs-rails/view_helpers.rb
@@ -1,24 +1,17 @@
 module CDNJS
   module ViewHelpers
     def cdnjs_include_tag(cdn_vars)
-      js_string_output = ''
 
+      js_string_output = Array.new
 
-      cdn_vars.each do |js_file|
-        js_file.each do |js_file_config|
+      cdn_vars.each do |js_file_config|
           
-          cdnjs = js_file_config['cdnjs']
-          localpath = js_file_config['localpath']
-          
-          js_string_output = [ 
-            javascript_include_tag("//cdnjs.cloudflare.com/ajax/libs/#{cdnjs}"),
-            javascript_tag("window.jQuery || document.write(unescape('#{javascript_include_tag(localpath).gsub('<','%3C')}'))")
-          ].join("\n").html_safe
-
-        end
+        js_string_output << javascript_include_tag("//cdnjs.cloudflare.com/ajax/libs/#{js_file_config['cdnjs']}")
+        js_string_output << javascript_tag("window.jQuery || document.write(unescape('#{javascript_include_tag(js_file_config['localpath']).gsub('<','%3C')}'))")
+      
       end
 
-      return js_string_output
+      return js_string_output.join("\n").html_safe
 
     end
   end
